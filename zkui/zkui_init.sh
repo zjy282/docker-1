7d483d04e738b64d418fc0a0230bc74890980df5
TT <wind@TTdeMacBook-Pro.local>
2018-07-30 11:09:03 +0800

    fix

--- zkui/zkui_init.sh
+++ zkui/zkui_init.sh
@@ -11,6 +11,10 @@ then
     exit 2
 fi
 
-sed -i "s/^zkServer/zkServer=$ZKHOST"
+sed -i "s/^zkServer/zkServer=$ZKHOST" /opt/zkui-master/config.cfg
+
+cd /opt/zkui-master
+mvn clean install
+nohup java -jar zkui-2.0-SNAPSHOT-jar-with-dependencies.jar &
 
 /usr/bin/supervisord
