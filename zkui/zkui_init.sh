318daf9ac745ac3f6fc3c64edfbb14e21bce0240
TT <wind@TTdeMacBook-Pro.local>
2018-07-30 11:05:10 +0800

    init

---
+++ zkui/zkui_init.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+if [[ ! -d /opt/zkui-master ]]
+then
+	unzip -q /opt/zkui.zip
+fi
+
+if [[ -z $ZKHOST ]]
+then
+    echo "ZKHOST is empty"
+    exit 2
+fi
+
+sed -i "s/^zkServer/zkServer=$ZKHOST"
+
+/usr/bin/supervisord
