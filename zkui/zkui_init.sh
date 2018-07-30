7e352e990fd56f65dffdb8b0395bc672bd2ab632
TT <wind@TTdeMacBook-Pro.local>
2018-07-30 11:22:01 +0800

    fix

--- zkui/zkui_init.sh
+++ zkui/zkui_init.sh
@@ -1,10 +1,5 @@
 #! /bin/sh
 
-if [[ ! -d /opt/zkui-master ]]
-then
-	unzip -q /opt/zkui.zip
-fi
-
 if [[ -z $ZKHOST ]]
 then
     echo "ZKHOST is empty"
@@ -13,8 +8,6 @@ fi
 
 sed -i "s/^zkServer/zkServer=$ZKHOST" /opt/zkui-master/config.cfg
 
-cd /opt/zkui-master
-mvn clean install
 nohup java -jar zkui-2.0-SNAPSHOT-jar-with-dependencies.jar &
 
 /usr/bin/supervisord
