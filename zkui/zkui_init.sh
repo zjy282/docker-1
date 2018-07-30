5711e1be9704dbadcead632d15a3e4aef42edf51
TT <wind@TTdeMacBook-Pro.local>
2018-07-30 11:47:31 +0800

    fix

--- zkui/zkui_init.sh
+++ zkui/zkui_init.sh
@@ -6,6 +6,6 @@ then
     exit 2
 fi
 
-sed -i "s/^zkServer/zkServer=$ZKHOST/g" /opt/zkui-master/config.cfg
+sed -i "s/^zkServer=localhost:2181,localhost:2181/zkServer=$ZKHOST/g" /opt/zkui-master/config.cfg
 
 /usr/bin/supervisord
