960a5effeb65eb4b880dbbb6168b6293d1fce95f
wind <guiqiang@qingsongchou.cojm>
2018-12-24 12:28:46 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM registry.cn-beijing.aliyuncs.com/wind-ali/supervisord
 WORKDIR /opt/
 
 COPY ./epel-release.rpm /opt/epel-release.rpm
-COPY ./webstatic-release.rpm /opt/webstatic-release.rpm
+COPY ./webtatic-release.rpm /opt/webtatic-release.rpm
 COPY ./init.sh /opt/init.sh
 COPY ./php-fpm.ini /etc/supervisord.d/php-fpm.ini
 RUN chmod +x /opt/init.sh
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 rpm -Uvh /opt/epel-release.rpm
-rpm -Uvh /opt/webstatic-release.rpm
+rpm -Uvh /opt/webtatic-release.rpm
 
 yum install -y gcc.x86_64 gcc-c++.x86_64 make.x86_64 php72w-cli.x86_64 php72w-devel.x86_64 php72w-fpm.x86_64 php72w-gd.x86_64 php72w-mbstring.x86_64 php72w-opcache.x86_64 php72w-pdo.x86_64 php72w-pear.noarch php72w-pecl-redis.x86_64 php72w-pecl-xdebug.x86_64 php72w-xml.x86_64
 
