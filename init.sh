6661d0c7379680ddd22e7e6c34deeda56417cfb9
wind <guiqiang@qingsongchou.cojm>
2018-12-28 14:57:06 +0800

    modify

--- init.sh
+++ init.sh
@@ -1,7 +1,34 @@
 #! /bin/sh
 
 sed -i "s/daemonize = yes/daemonize = no/g" /etc/php-fpm.conf
-pecl install grpc
-pecl install yaf
+
+function yaf() {
+    pecl install yaf
+    echo "extension=yaf.so" >> /etc/php.d/yaf.ini
+    echo "yaf.cache_config=1" >> /etc/php.d/yaf.ini
+    echo "yaf.use_namespace=1" >> /etc/php.d/yaf.ini
+    if [[ -z "${YAF_ENV}" ]] 
+    then
+        echo -e "\nyaf.environ=\"development\"" >> /etc/php.d/yaf.ini
+    else
+        echo -e "\nyaf.environ=\"${YAF_ENV}\"" >> /etc/php.d/yaf.ini
+    fi
+}
+
+function grpc() {
+    pecl install grpc
+    echo "extension=grpc.so" >> /etc/php.d/grpc.so
+}
+
+if [[ ${#MODULES[@]} -gt 0 ]]
+then
+    for module in ${MODULES[@]}
+    do
+        if [ "$(type -t $module)" = "function" ]
+        then
+            $module
+        fi;
+    done
+fi
 
 supervisord
