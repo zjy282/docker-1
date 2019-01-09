94ee2c3e4a1d5f7a449fdd15ed363f13b092c5a8
桂强 <guiqiang@qingsongchou.com>
2019-01-09 13:10:35 +0800

    init

--- init.sh
+++ init.sh
@@ -5,19 +5,21 @@ sed -i 's/^#remoteip/remoteip/' /etc/pptpd.conf
 sed -i 's/^#ms-dns/ms-dns/g' /etc/ppp/options.pptpd
 
 touch /etc/sysctl.conf
-sed -i 'anet.ipv4.ip_forward=1' /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
 pass=$RANDOM 
-sed -i "avpn pptpd $RANDOM *" /etc/ppp/chap-secrets
+echo "vpn pptpd $pass *" > /etc/ppp/chap-secrets
 
 unzip /opt/shadowsocks-server.zip -d /opt
 
-sed -i "acommand=/opt/shadowsocks-server -p 2333 -k $pass -m aes-256-cfb -t 10" /etc/supervisord.d/shadow.ini
+echo "command=/opt/shadowsocks-server -p 2333 -k $pass -m aes-256-cfb -t 10" >> /etc/supervisord.d/shadow.ini
 
+echo ""
 echo "password:$pass"
 echo "port:2333"
 echo "encryption:aes-256-cfb"
+echo ""
 
 pptpd
 /usr/bin/supervisord -c /etc/supervisord.conf
